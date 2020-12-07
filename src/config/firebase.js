2a410f0ca53040ba7b11510a5b46e93647bc77e8
joeappleton18 <joe.appleton@solent.ac.uk>
2020-12-07 11:41:02 +0000

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,7 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDIaDCr2LcFa-aElRRKOPE81qvUSae3Dws",
-    authDomain: "web-dev-2020.firebaseapp.com",
-    databaseURL: "https://web-dev-2020.firebaseio.com",
-    projectId: "web-dev-2020",
-    storageBucket: "web-dev-2020.appspot.com",
-    messagingSenderId: "524267669677",
-    appId: "1:524267669677:web:56b90ce7420a5d24805e24"
+   /**
+   updated with your application config
+   **/
   };
 
 
